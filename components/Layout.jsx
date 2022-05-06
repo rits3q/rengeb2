4d0d2662027cd447fbceb30fed16223bcc35aa1e
Ritesh <riteshsinghgzp2016@gmail.com>
2022-05-06 14:42:00 +0530

    about & home

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,17 @@
+import Head from "next/dist/shared/lib/head"
+
 const Layout = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <div>
+    <Head>
+    <link rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
+   
+    </Head>
+ {children}
+ </div>
+  )
 };
 
 export default Layout;
